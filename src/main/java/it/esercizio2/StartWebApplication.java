148c2def87db054b53993a22f85114029acb1850
DanieleMaglio <danielemaglio98@gmail.com>
2019-09-16 12:13:25 +0200

    main

---
+++ src/main/java/it/esercizio2/StartWebApplication.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.esercizio2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Utente
+ */
+@SpringBootApplication
+public class StartWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartWebApplication.class,args);
+    }
+}
